6561774c197901f8591416c0e3bcccdcff37b406
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-06-29 14:01:13 +0100

    Fix GraphQL author mutation nullability

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -2,50 +2,95 @@ import Author from "../models/Author";
 import Category from "../models/Category";
 import Book from "../models/Book";
 
+const formatAuthor = (author: any) => ({
+  id: author?._id?.toString() ?? author?.id ?? "",
+  name: author?.name ?? "",
+  biography: author?.biography ?? "",
+  country: author?.country ?? "",
+});
+
+const formatCategory = (category: any) => ({
+  id: category?._id?.toString() ?? category?.id ?? "",
+  name: category?.name ?? "",
+});
+
+const formatBook = (book: any, author: any, category: any) => ({
+  id: book?._id?.toString() ?? book?.id ?? "",
+  title: book?.title ?? "",
+  description: book?.description ?? "",
+  publishedYear: book?.publishedYear ?? 0,
+  pages: book?.pages ?? 0,
+  author: formatAuthor(author),
+  category: formatCategory(category),
+});
+
 export const resolvers = {
   Query: {
-    // Tous les auteurs
     authors: async () => {
-      return await Author.find();
+      const authors = await Author.find().lean();
+      return authors.map(formatAuthor);
     },
 
-    // Un seul auteur
     author: async (_: any, { id }: { id: string }) => {
-      return await Author.findById(id);
+      const author = await Author.findById(id).lean();
+      return author ? formatAuthor(author) : null;
     },
 
-    // Toutes les catégories
     categories: async () => {
-      return await Category.find();
+      const categories = await Category.find().lean();
+      return categories.map(formatCategory);
     },
 
-    // Une seule catégorie
     category: async (_: any, { id }: { id: string }) => {
-      return await Category.findById(id);
+      const category = await Category.findById(id).lean();
+      return category ? formatCategory(category) : null;
     },
 
-    // Tous les livres
     books: async () => {
-      return await Book.find();
+      const books = await Book.find().lean();
+      return await Promise.all(
+        books.map(async (book: any) => {
+          const author = book.author ? await Author.findById(book.author).lean() : null;
+          const category = book.category ? await Category.findById(book.category).lean() : null;
+          return formatBook(book, author, category);
+        })
+      );
     },
 
-    // Un seul livre
     book: async (_: any, { id }: { id: string }) => {
-      return await Book.findById(id);
+      const book = await Book.findById(id).lean();
+      if (!book) return null;
+
+      const author = book.author ? await Author.findById(book.author).lean() : null;
+      const category = book.category ? await Category.findById(book.category).lean() : null;
+      return formatBook(book, author, category);
     },
   },
+
   Mutation: {
-    createAuthor : async (_: any, { input }: any)=>{
-           const author = await Author.create(input);
-    return author;
+    createAuthor: async (_: any, { input }: any) => {
+      const author = await Author.create(input);
+      const authorData = author.toObject() as { biography?: string; country?: string };
+
+      return formatAuthor({
+        ...input,
+        ...authorData,
+        biography: input?.biography ?? authorData?.biography ?? "",
+        country: input?.country ?? authorData?.country ?? "",
+      });
     },
-    createCategory : async(_: any, { input }: any )=>{
-        const category = await Category.create(input);
-        return category;
+
+    createCategory: async (_: any, { input }: any) => {
+      const category = await Category.create(input);
+      return formatCategory(category.toObject());
     },
+
     createBook: async (_: any, { input }: any) => {
-  const book = await Book.create(input);
-  return book;
-},
-  }
+      const createdBook = await Book.create(input);
+      const book = await Book.findById(createdBook._id).lean();
+      const author = book?.author ? await Author.findById(book.author).lean() : null;
+      const category = book?.category ? await Category.findById(book.category).lean() : null;
+      return book ? formatBook(book, author, category) : null;
+    },
+  },
 };
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,57 +1,61 @@
-import { gql } from "graphql-tag";
+export const typeDefs = `
 
-export const typeDefs = gql`
-  type Author {
-    id: ID!
-    name: String!
-    bio: String
-  }
+type Author {
+  id: ID!
+  name: String!
+  biography: String!
+  country: String!
+}
 
-  type Category {
-    id: ID!
-    name: String!
-    description: String
-  }
+type Category {
+  id: ID!
+  name: String!
+}
 
-  type Book {
-    id: ID!
-    title: String!
-    description: String
-    publishedYear: Int
-    author: ID!
-    category: ID!
-  }
+type Book {
+  id: ID!
+  title: String!
+  description: String!
+  publishedYear: Int!
+  pages: Int!
+  author: Author!
+  category: Category!
+}
 
-  input CreateAuthorInput {
-    name: String!
-    bio: String
-  }
+input CreateAuthorInput {
+  name: String!
+  biography: String!
+  country: String!
+}
 
-  input CreateCategoryInput {
-    name: String!
-    description: String
-  }
+input CreateCategoryInput {
+  name: String!
+}
 
-  input CreateBookInput {
-    title: String!
-    description: String
-    publishedYear: Int
-    author: ID!
-    category: ID!
-  }
+input CreateBookInput {
+  title: String!
+  description: String!
+  publishedYear: Int!
+  pages: Int!
+  author: ID!
+  category: ID!
+}
 
-  type Query {
-    authors: [Author!]!
-    author(id: ID!): Author
-    categories: [Category!]!
-    category(id: ID!): Category
-    books: [Book!]!
-    book(id: ID!): Book
-  }
+type Query {
+  authors: [Author!]!
+  author(id: ID!): Author
 
-  type Mutation {
-    createAuthor(input: CreateAuthorInput!): Author!
-    createCategory(input: CreateCategoryInput!): Category!
-    createBook(input: CreateBookInput!): Book!
-  }
+  categories: [Category!]!
+  category(id: ID!): Category
+
+  books: [Book!]!
+  book(id: ID!): Book
+}
+
+type Mutation {
+  createAuthor(input: CreateAuthorInput!): Author!
+  createCategory(input: CreateCategoryInput!): Category!
+  createBook(input: CreateBookInput!): Book!
+}
 `;
+console.log("Schema loaded");
